1a85aa4604ae475ac127a308e1ca3b568c6cfe54
suvojitd79@gmail.com <suvojitd79@gmail.com>
2019-10-22 21:15:53 +0530

    CreateParkingLot constructor added

--- src/model/parkinglot.go
+++ src/model/parkinglot.go
@@ -3,9 +3,22 @@ package model
 //ParkingLot -
 type ParkingLot struct {
 	size                        uint64 // specify the size of the parking lot (N)
-	slots                       []ParkingSlot
+	slots                       []Slot
 	isAvailable                 bool                // whether the lot is full or not
 	colorRegistrationNumbesDict map[string][]string // returns a list of registrations on passing a color as a key (color - > reg ids)
 	colorSlotsDict              map[string][]uint64 // returns the slot ids for a specific color (color -> slots)
 	registrationNumbeSlotDict   map[string]string   // returns the slot number for a specific registration number
+}
+
+//CreateParkingLot - works as a constructor for the ParkingLot
+func CreateParkingLot(size uint64) *ParkingLot {
+
+	parkingLot := ParkingLot{}
+	parkingLot.size = size
+	parkingLot.slots = []Slot{}
+	parkingLot.isAvailable = (size > 0)
+	parkingLot.colorRegistrationNumbesDict = map[string][]string{}
+	parkingLot.colorSlotsDict = map[string][]uint64{}
+	parkingLot.registrationNumbeSlotDict = map[string]string{}
+	return &parkingLot
 }
--- src/model/slot.go
+++ src/model/slot.go
@@ -1,28 +1,28 @@
 package model
 
-//ParkingSlot - conatins the information about a specific spot of the parking lot
-type ParkingSlot struct {
+//Slot - conatins the information about a specific spot of the parking lot
+type Slot struct {
 	id          uint64  // slot id - [+ve number from 1....N]
 	isAvailable bool    // specify whether the spot is available
 	vehicle     Vehicle // contains the info of the parked vehicle
 }
 
 //ID - getter for id
-func (slot ParkingSlot) ID() uint64 {
+func (slot Slot) ID() uint64 {
 	return slot.id
 }
 
 //IsAvailable - getter for isAvailable
-func (slot ParkingSlot) IsAvailable() bool {
+func (slot Slot) IsAvailable() bool {
 	return slot.isAvailable
 }
 
 // GetVehicle  - getter for vehicle
-func (slot ParkingSlot) GetVehicle() Vehicle {
+func (slot Slot) GetVehicle() Vehicle {
 	return slot.vehicle
 }
 
 //SetAvailable - set the availability (removes/adds car)
-func (slot *ParkingSlot) SetAvailable(value bool) {
+func (slot *Slot) SetAvailable(value bool) {
 	slot.isAvailable = value
 }
